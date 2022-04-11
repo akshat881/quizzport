86b36895f3c6aedba208143a58b6e9993ff20bbb
AKSHAT <57553607+akshat881@users.noreply.github.com>
2022-04-11 10:02:56 +0530

    Update data.php

--- data.php
+++ data.php
@@ -1,1 +1,11 @@
+<?php
+$user = "root";
+$pass = "";
+$name = "localhost";
+$dbn="answ";
+$con=mysqli_connect($name,$user,$pass,$dbn);
+$db="CREATE TABLE akshat(name VARCHAR(50),regi INT(100))";
 
+
+
+?>
